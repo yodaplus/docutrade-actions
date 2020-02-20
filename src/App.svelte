d67c7de678749b22b0ae6f53ea309bce6d4ad492
Nebulis <nebounet@gmail.com>
2020-02-21 00:56:51 +0800

    feat: use undraw images

--- src/App.svelte
+++ src/App.svelte
@@ -18,14 +18,17 @@
     }, 1000);
   }
   $: if (timer === 0) {
+    clearInterval(interval);
     window.location.href = `${action.payload.redirect}${location.search}`
   }
 </script>
 
 <style>
   .container {
+    height: 100%;
     display: flex;
     align-items: center;
+    justify-content: center;
     flex-direction: column;
   }
   .container > div {
@@ -35,27 +38,46 @@
     text-align: center;
   }
   .icon {
-    width: 200px;
+    width: 50vw;
+    min-width: 200px;
+    max-width: 600px;
   }
   .text {
-    font-size: 2rem;
+    text-align: center;
+    font-size: 1.5rem;
+  }
+  .error {
+    color: #ff6584;
+    font-weight: 500;
+  }
+  .success {
+    color: #6c63ff;
+    font-weight: 500;
   }
 </style>
 
 <main>
   {#if valid}
     <div class="container">
-      <div class="img-container">
-        <img src="./send.svg" class="icon" alt="redirect" />
+      <div class="text">
+        Redirecting to
+        <span class="success">{action.payload.redirect}</span>
+        in {timer}
       </div>
-      <div class="text">Redirecting to {action.payload.redirect} in {timer}</div>
+      <div class="img-container">
+        <img src="./undraw_online_transactions_02ka.png" class="icon" alt="redirect" />
+      </div>
     </div>
   {:else}
     <div class="container">
-      <div class="img-container">
-        <img src="./x-octagon.svg" class="icon" alt="redirect" />
+      <div class="text">
+        <span class="error">{action.payload.redirect}</span>
+        <br />
+        is not an authorized platform.
       </div>
-      <div class="text">{action.payload.redirect} is not an authorized platform by open-attestation.</div>
+      <div class="img-container">
+        <img src="./undraw_cancel_u1it.png" class="icon" alt="redirect" />
+      </div>
     </div>
   {/if}
 </main>
