07bfeb786ffbabc30005bc4aa232ce3ea76e2c8b
Nebulis <nebounet@gmail.com>
2020-02-21 14:36:39 +0800

    fix: oopsie

--- src/App.svelte
+++ src/App.svelte
@@ -24,12 +24,8 @@
   }
   $: if (timer === 0) {
     clearInterval(interval);
-    // window.location.href = `${action.payload.redirect}${location.search}`;
+    window.location.href = `${action.payload.redirect}${location.search}`;
   }
-  /**
-   *
-  https://action.openattestation.com/?q=%7B%22type%22:%22DOCUMENT%22,%22payload%22:%7B%22uri%22:%22https://api.myjson.com/bins/1a9acm%22,%22key%22:%221b8c334a38f9ff96108303a4ba0cc592f1559eb24f5b48b70c9300c60a34d5e9%22,%22permittedAction%22:%5B%22STORE%22%5D,%22redirect%22:%22https://dev.opencerts.io%22%7D%7D
-   */
 </script>
 
 <style>
